3eeafcc3fbdfd368dc1055f9800efb335f219b91
UIMSolutions <ui.manufaktur@gmail.com>
2026-03-24 07:03:27 +0100

    Refactor library structure and update dependencies for SAP libraries

---
+++ source/uim/sap/libraries/package.d
@@ -0,0 +1,9 @@
+module uim.sap.libraries;
+
+public {
+    import uim.sap.library;
+    // import uim.sap.libraries.idoc;
+    // import uim.sap.libraries.oauth;
+    // import uim.sap.libraries.odata;
+    // import uim.sap.libraries.rfc;
+}
